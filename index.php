81e1eb49e4671f3f83430cf894934936468b8576
Felix Aluchio <felixaluchio@gmail.com>
2026-04-22 14:19:46 +0300

    Updated index page layout and functionality

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 if(!isset($_SESSION['username'])){
-  header("Location: users.php");
+  header("Location: login.php");
+  exit;
 }
 ?>
 <!DOCTYPE html>
@@ -11,11 +12,16 @@ if(!isset($_SESSION['username'])){
   <script src="bootstrap-5.3.8-dist/js/bootstrap.min.js"></script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+  <title>Home</title>
 </head>
 <body>
   <?php include 'links.php'; ?>
   <div class="container">
-  
+    <div class ="container mt-4">
+      <div = "alert alert-success">
+        Welcome back, <strong><?php echo htmlspecialchars($_SESSION['fullname']); ?></strong>
+    </div>
+  </div>
+  </div>
 </body>
 </html>
